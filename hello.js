f6e1767f993aca40402d09113a0fe7d062750978
moraruv0 <75850366+moraruv0@users.noreply.github.com>
2020-12-18 20:53:15 +0200

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,20 @@
+var time = 3000
+time=1000
+var message= "Bine ai venit!"
+var isLate = false
+
+if (isLate) {
+    alert("go home")
+}
+
+if (isLate) { alert("go home")
+    
+} else { alert("Contiue")
+    
+}
+setTimeout(welcome, time)
+// set_timeout()
+// function welcome() {
+//     alert(message)
+//     alert("Welocome")
+// }
